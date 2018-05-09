54305928306650b838810fd616698a4ea2a710bd
Katie Patterson <kirska@kirska.com>
2018-05-08 21:48:04 -0500

    Fetching animals from ShelterLuv

---
+++ ShelterLuv_JSON_Sync.py
@@ -0,0 +1,61 @@
+# ShelterLuv sync to local Dallas Pets Alive filesystem script
+# Developed for Dallas Pets Alive by Katie Patterson www.kirska.com
+import requests, collections
+
+API_KEY = '4b5a377b-b876-4a20-8d82-ebeac0b10d51'
+
+# First fetch the entire list of animals
+
+headers = {'x-api-key': API_KEY}
+offset = 0
+animals_dict = {}
+total_count = 0
+
+while 1:
+    url = 'https://www.shelterluv.com/api/v1/animals?status_type=publishable&offset=' + str(offset)
+    print 'fetching ' + url
+    response = requests.get(url, headers=headers)
+
+    # check http response code
+    if response.status_code != 200:
+        print 'invalid response code'
+        exit(1)
+
+    response_json = response.json()
+
+    if response_json["success"] != 1:
+        print 'invalid attempt'
+        exit(2)
+
+    total_count = response_json["total_count"]
+
+    if total_count == 0:
+        print 'no animals found - error'
+        exit(3)
+
+    # add each animal to the dict
+    for animal in response_json["animals"]:
+        name = animal["Name"]
+        if name in animals_dict:
+            print 'animal already exists'
+            continue
+
+        animals_dict[name] = animal
+
+    # check for more animals
+    if response_json["has_more"]:
+        offset += 100
+    else:
+        break
+
+# we should have all the animals now
+if str(animals_dict.__len__()) != str(total_count):
+    print 'something went wrong'
+
+# sort the list of animals by name
+ordered_animals = collections.OrderedDict(sorted(animals_dict.items()))
+
+
+
+
+
