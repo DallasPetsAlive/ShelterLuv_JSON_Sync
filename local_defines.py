21fb3680378442352545d1cfe5c009e6ed4a5a2b
Katie Patterson <kpatterson67@gmail.com>
2021-04-06 23:10:33 -0500

    restore local_defines

---
+++ local_defines.py
@@ -0,0 +1,19 @@
+# Local defines for ShelterLuv JSON sync - vary by installation
+# Developed for Dallas Pets Alive by Katie Patterson www.kirska.com
+
+PROFILES_DIRECTORY = "C:\\wamp64\\www\\wordpress\\wp-content\\themes\\Divi-child\\pet_data\\profiles\\"
+
+DOG_LIST_FILE = "C:\\wamp64\\www\\wordpress\\wp-content\\themes\\Divi-child\\pet_data\\listings\\dog_list.php"
+
+CAT_LIST_FILE = "C:\\wamp64\\www\\wordpress\\wp-content\\themes\\Divi-child\\pet_data\\listings\\cat_list.php"
+
+OTHER_LIST_FILE = "C:\\wamp64\\www\\wordpress\\wp-content\\themes\\Divi-child\\pet_data\\listings\\other_list.php"
+
+PLACEHOLDER_IMAGE = "\\wordpress\\wp-content\\themes\\Divi-child\\images\\dpa-placeholder.png"
+
+LIST_THEME_PATH = "../../wordpress/wp-content/themes/Divi-child/"
+
+PET_LINK_RELATIVE_PATH = "../"
+
+API_KEY = ''
+ANIMALS_FILE = 'C:\\wamp64\\www\\wordpress\\wp-content\\themes\\Divi-child\\pet_data\\animals.json'
